bef84fa88ec0b3ec0209fb739990a3d0069a7878
Ilya Petrov <dnamix1@gmail.com>
2020-05-19 15:52:01 +0300

    main cells range selection login in the Table Component

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -1,7 +1,10 @@
 import { ExcelComponent } from '../../core/ExcelComponent';
 import { createTable } from './table.template';
 import { resizeHandler } from './table.resize';
-import { shouldResize } from './table.functions';
+import { shouldResize, isCell } from './table.functions';
+import { TableSelection } from './TableSelection';
+import { range, matrix } from '../../core/utils';
+import { $ } from '@core/dom';
 export class Table extends ExcelComponent {
   static className = 'excel__table';
   constructor($root) {
@@ -12,9 +15,29 @@ export class Table extends ExcelComponent {
   toHTML() {
     return createTable();
   }
+
+  prepare() {
+    this.selection = new TableSelection();
+  }
+
+  init() {
+    super.init();
+    const $cell = this.$root.findOneCell('[data-id="1:0"]');
+    this.selection.select($cell);
+  }
   onMousedown(event) {
     if (shouldResize(event)) {
       resizeHandler(this.$root, event);
+    } else if (isCell(event)) {
+      const $target = $(event.target);
+      if (event.shiftKey) {
+        const $cells = matrix($target, this.selection.current).map((id) =>
+          this.$root.findOneCell(`[data-id="${id}"]`)
+        );
+        this.selection.selectGroup($cells);
+      } else {
+        this.selection.select($target);
+      }
     }
   }
 }
