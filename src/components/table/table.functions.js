c2729534848522098c2f3ee5c45cdb52a411c2c6
Ilya Petrov <dnamix1@gmail.com>
2020-05-20 20:41:38 +0300

    function to restrict movement in the table by end-user with predefined set of keys (mimicking excel)

--- src/components/table/table.functions.js
+++ src/components/table/table.functions.js
@@ -5,3 +5,25 @@ export function shouldResize(event) {
 export function isCell(event) {
   return event.target.dataset.type === 'cell';
 }
+
+export function nextSelector(key, { col, row }) {
+  const MIN_COL = 0;
+  const MIN_ROW = 1;
+  switch (key) {
+    case 'Enter':
+    case 'ArrowDown':
+      row++;
+      break;
+    case 'Tab':
+    case 'ArrowRight':
+      col++;
+      break;
+    case 'ArrowLeft':
+      col = col - 1 < MIN_COL ? MIN_COL : col - 1;
+      break;
+    case 'ArrowUp':
+      row = row - 1 < MIN_ROW ? MIN_ROW : row - 1;
+      break;
+  }
+  return `[data-id="${row}:${col}"]`;
+}
