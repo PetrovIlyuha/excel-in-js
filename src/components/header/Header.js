74730adc9593b2008779ceea8218687505637a41
Ilya Petrov <dnamix1@gmail.com>
2020-05-28 11:05:27 +0300

    Page factory class and Header buttons activation

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -3,13 +3,14 @@ import { changeTitle } from '../../redux/actions';
 import { defaultTitle } from '../../constants';
 import { $ } from '../../core/dom';
 import { debounce } from '../../core/utils';
+import { ActiveRoute } from '../../core/router/ActiveRoute';
 
 export class Header extends ExcelComponent {
   static className = 'excel__header';
   constructor($root, options) {
     super($root, {
       name: 'Header',
-      listeners: ['input'],
+      listeners: ['input', 'click'],
       ...options,
     });
   }
@@ -20,15 +21,15 @@ export class Header extends ExcelComponent {
 
   toHTML() {
     const title = this.store.getState().title || defaultTitle;
-    return `    
+    return `
       <input type="text" class="input" value="${title}" />
 
       <div class="excel__header__btn-group">
-        <div class="button">
-          <i class="material-icons">delete</i>
+        <div class="button" data-button="remove">
+          <i class="material-icons" data-button="remove">delete</i>
         </div>
-        <div class="button">
-          <i class="material-icons">exit_to_app </i>
+        <div class="button" data-button="exit">
+          <i class="material-icons" data-button="exit">exit_to_app </i>
         </div>
       </div>
     `;
@@ -37,5 +38,18 @@ export class Header extends ExcelComponent {
   onInput(event) {
     const $target = $(event.target);
     this.$dispatch(changeTitle($target.text()));
+  }
+
+  onClick(event) {
+    const $target = $(event.target);
+    if ($target.data.button === 'remove') {
+      const decision = confirm('Are you sure you want to delete this Table?');
+      if (decision) {
+        localStorage.removeItem('table:' + ActiveRoute.params);
+        ActiveRoute.navigate('');
+      }
+    } else if ($target.data.button === 'exit') {
+      ActiveRoute.navigate('');
+    }
   }
 }
