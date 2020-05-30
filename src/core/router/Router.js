bde05705eb365f0ec7d72508565bf5eb3e9b7cf0
Ilya Petrov <dnamix1@gmail.com>
2020-05-30 13:12:53 +0300

    more versatile Router with loader while page changes

--- src/core/router/Router.js
+++ src/core/router/Router.js
@@ -1,5 +1,6 @@
 import { $ } from '../dom';
 import { ActiveRoute } from './ActiveRoute';
+import { Loader } from '../../components/Loader';
 
 export class Router {
   constructor(selector, routes) {
@@ -8,6 +9,8 @@ export class Router {
     }
     this.$placeholder = $(selector);
     this.routes = routes;
+
+    this.loader = new Loader();
     this.changePageHandler = this.changePageHandler.bind(this);
     this.page = null;
     this.init();
@@ -18,16 +21,18 @@ export class Router {
     this.changePageHandler();
   }
 
-  changePageHandler() {
+  async changePageHandler() {
     if (this.page) {
       this.page.destroy();
     }
-    this.$placeholder.clear();
+    this.$placeholder.clear().append(this.loader);
+
     const Page = ActiveRoute.path.includes('excel')
       ? this.routes.excel
       : this.routes.dashboard;
     this.page = new Page(ActiveRoute.params);
-    this.$placeholder.append(this.page.getRoot());
+    const root = await this.page.getRoot();
+    this.$placeholder.clear().append(root);
     this.page.afterRender();
   }
 
